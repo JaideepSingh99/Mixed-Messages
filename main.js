52810e0d69837ac4aa0257f4b3e91fd326663a1c
JaideepSingh99 <jai46560@gmail.com>
2024-08-03 14:37:28 +0530

    Add random message function

--- main.js
+++ main.js
@@ -2,3 +2,23 @@
 const randNum = (num) => {
     return Math.floor(Math.random() * num);
 }
+
+// 
+const randMessage = () => {
+    const astroSign = ['Air sign', 'Water sign', 'Fire sign', 'Earth sign'];
+    const react = ['Congrats', 'Sucks to be you', 'Meh', 'Hehe'];
+    const meme = ["You'll die because of ligma!!", "You have a friend name Candice", "You like seeing CD."];
+
+    const message = [];
+
+    randAstro = astroSign[randNum(astroSign.length)];
+    message.push(randAstro);
+    randReact = react[randNum(react.length)];
+    message.push(randReact);
+    randmeme = meme[randNum(meme.length)];
+    message.push(randmeme);
+
+    return (`You're ` + message.join(' '));
+}
+
+console.log(randMessage());
